1f5a52a8ac7ffcd71b52bc8cb591b37342c3935b
Nworah-Gabriel <gabrielnworah6@gmail.com>
2023-01-06 15:15:40 +0100

    Development in progress

--- lib/poll.dart
+++ lib/poll.dart
@@ -3,6 +3,7 @@ import 'package:vote/details.dart';
 import 'package:vote/home.dart';
 import 'package:vote/result.dart';
 import 'package:vote/vote_page.dart';
+import 'package:vote/voters_registration.dart';
 
 enum Selected { Downloadable, Deliverable }
 
@@ -34,9 +35,40 @@ class _PollState extends State<Poll> {
   Object? _value = {
     "value": "",
   };
+  late int i;
   // late String name;
   // name = _value;
   Selected? _selected;
+  @override
+  Widget Render() {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+      child: Container(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+          child: RadioListTile(
+              title: Text(Selected.Deliverable.name),
+              value: Selected.Downloadable,
+              groupValue: _selected,
+              onChanged: (val) {
+                setState(() {
+                  _selected = val;
+                });
+              }),
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.green,
+              ),
+              borderRadius: BorderRadius.circular(10))),
+    );
+  }
+
+  void getit(int num) {
+    int i;
+    for (i = 0; i < num; i += 1) {
+      Render();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -81,60 +113,107 @@ class _PollState extends State<Poll> {
                 }, // The `_incrementCounter` function
               ),
             ]),
-        body: Container(
-            child: Column(children: [
-          SizedBox(
-            height: 60,
-          ),
-          Column(
-            children: [
-              Container(
-                child: Center(
+        body: SingleChildScrollView(
+            child: Container(
+          child: Column(children: [
+            SizedBox(
+              height: 60,
+            ),
+            Column(
+              children: [
+                Container(
+                  child: Center(
+                      child: Text(
+                    "Cast your vote!",
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.normal,
+                        color: Colors.white),
+                  )),
+                  height: 140,
+                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color.fromARGB(255, 98, 209, 102),
+                      boxShadow: [
+                        BoxShadow(color: Colors.grey, blurRadius: 40.0),
+                      ]),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            // Render(),
+            // Render(),
+            for (i = 0; i < 10; i += 1) Render(),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegisterVoter(),
+                          ));
+                    },
                     child: Text(
-                  "Cast your vote!",
-                  style: TextStyle(
-                      fontSize: 40.0,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.normal,
-                      color: Colors.white),
-                )),
-                height: 140,
-                margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                decoration: const BoxDecoration(
+                      'Submit',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  height: 50,
+                  width: 130,
+                  decoration: const BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Color.fromARGB(255, 98, 209, 102),
+                    color: Color.fromARGB(255, 80, 246, 86),
                     boxShadow: [
-                      BoxShadow(color: Colors.grey, blurRadius: 40.0),
-                    ]),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          RadioListTile(
-              value: Selected.Deliverable,
-              groupValue: _selected,
-              onChanged: (val) {
-                setState(() {
-                  _selected = val;
-                });
-              }),
-          RadioListTile(
-              title: Text(Selected.Downloadable.name),
-              value: Selected.Downloadable,
-              groupValue: _selected,
-              onChanged: (val) {
-                setState(() {
-                  _selected = val;
-                });
-              }),
-          RadioListTile(value: 3, groupValue: 1, onChanged: null),
-          RadioListTile(value: 4, groupValue: 1, onChanged: null),
-          RadioListTile(value: 5, groupValue: 1, onChanged: null),
-        ])));
+                      BoxShadow(color: Colors.grey, blurRadius: 3.0),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Vote(),
+                          ));
+                    },
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  height: 50,
+                  width: 130,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.red,
+                    boxShadow: [
+                      BoxShadow(color: Colors.grey, blurRadius: 3.0),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ]),
+        )));
   }
 }
