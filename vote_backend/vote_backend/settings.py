ed9af6d5c4ec5b7c641f3b7db556524dfe5f4477
Nworah-Gabriel <gabrielnworah6@gmail.com>
2023-01-13 06:05:36 +0100

    worked on the api

--- vote_backend/vote_backend/settings.py
+++ vote_backend/vote_backend/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*=0aa+i(2ca#u^9+aa8v$u3n*$ja97xal1se0xcgv1$pipr^eg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iamsaggio.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
